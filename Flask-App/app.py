ea93802f643cd204302c8e9b57aaf07e952c80e8
Ajinkya Temgire <ajinkya.temgire0835@gmail.com>
2024-08-04 16:20:03 +0530

    created flask app

---
+++ Flask-App/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template,request
+import mlflow
+from mlflow import MlflowClient
+from preprocessing_utility import normalize_text
+import dagshub
+import pickle
+
+mlflow.set_tracking_uri("https://dagshub.com/AJINKYA-TEMGIRE/Emotion_Detection.mlflow")
+dagshub.init(repo_owner='AJINKYA-TEMGIRE', repo_name='Emotion_Detection', mlflow=True)
+
+app = Flask(__name__)
+
+# load model from model registry
+def get_latest_model_version(model_name):
+    client = MlflowClient()
+    latest_version = client.get_latest_versions(model_name, stages=["Production"])
+    if not latest_version:
+        latest_version = client.get_latest_versions(model_name, stages=["None"])
+    return latest_version[0].version if latest_version else None
+
+model_name = "Logistic Regression"
+model_version = get_latest_model_version(model_name)
+
+model_uri = f'models:/{model_name}/{model_version}'
+model = mlflow.pyfunc.load_model(model_uri)
+
+vectorizer = pickle.load(open('models/vectorizer.pkl','rb'))
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html',result=None)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    text = request.form['text']
+
+    # clean
+    text = normalize_text(text)
+
+    # bow
+    features = vectorizer.transform([text])
+
+    # prediction
+    result = model.predict(features)
+
+    # show
+    return render_template('index.html', result= result[0])
+
+app.run(debug=True)
